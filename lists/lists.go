76588f9308c0c220bb643728b7bafee56e3652a0
hovertank3d <iskrim666@gmail.com>
2024-01-25 17:39:32 +0100

    stdfp: cleanup and add Map function

--- lists/lists.go
+++ lists/lists.go
@@ -1,1 +1,13 @@
 package lists
+
+type List[T any] interface {
+	Part(int, int) FiniteList[T]
+	Slice(int, int) []T
+	Element(int) T
+}
+type FiniteList[T any] interface {
+	List[T]
+	// Size returns size of finite list
+	// for infinite it should return -1
+	Size() int
+}
--- stdfp.go
+++ stdfp.go
@@ -1,9 +1,18 @@
 package stdfp
 
-import "github.com/hovertank3d/stdfp/lists"
+import (
+	"github.com/hovertank3d/stdfp/lists"
+	"github.com/hovertank3d/stdfp/lists/list"
+)
 
 func ForEach[T any](list lists.FiniteList[T], f func(int, T)) {
 	for i := 0; i < list.Size(); i++ {
 		f(i, list.Element(i))
 	}
+}
+
+func Map[T any](l lists.List[T], m func(T) T) lists.List[T] {
+	return list.New(func(i int) T {
+		return m(l.Element(i))
+	})
 }
